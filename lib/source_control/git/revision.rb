3404f342bbb0cd5adf19689c4143f9a755c890c1
Captain Cruise <build@weplay-build.local>
2008-08-11 15:35:56 -0400

    revision to_s support

--- lib/source_control/git/revision.rb
+++ lib/source_control/git/revision.rb
@@ -2,10 +2,18 @@ module SourceControl
   class Git
     class Revision < AbstractRevision
 
-      attr_reader :number, :author, :time 
+      attr_reader :number, :author, :time, :message, :changeset 
 
-      def initialize(number, author, time)
-        @number, @author, @time = number, author, time
+      def initialize(number, author = nil, time = nil, message = nil, changeset = nil)
+        @number, @author, @time = number, author, time, message, changeset
+      end
+
+      def to_s
+      	<<-EOL
+Revision #{number} committed by #{author} on #{time.strftime('%Y-%m-%d %H:%M:%S') if time}
+#{message}
+#{changeset ? changeset.collect { |entry| entry.to_s }.join("\n") : nil}
+	EOL
       end
 
       def ==(other)
